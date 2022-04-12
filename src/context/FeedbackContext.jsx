bbadd45fc515f0590b3228bb334e830eed5ee6df
Anurag Pramanik <p.anurag@outlook.com>
2022-04-12 23:30:54 +0530

    add json-server and fetch api

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,16 +1,30 @@
 import { v4 as uuidv4 } from 'uuid'
-import { createContext, useState } from 'react'
-import FeedbackData from '../data/FeedbackData'
+import { createContext, useState, useEffect } from 'react'
+// import FeedbackData from '../data/FeedbackData'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState(FeedbackData)
-
+  const [isLoading, setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([])
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   })
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+
+  // Fetch feedback from mock backend
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      `http://localhost:5000/feedback?_sort=id&_order=desc`
+    )
+    const data = await response.json()
+
+    setFeedback(data)
+    setIsLoading(false)
+  }
 
   // Add feedback
   const addFeedback = (newFeedback) => {
@@ -45,6 +59,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
